6a058e197fba6f02b0c4552130f8d749908e090b
Nattawat Hayamin <INatZaza@hotmail.com>
2024-05-15 11:48:49 +0700

    try apply transition for ios

--- script/master.js
+++ script/master.js
@@ -2,6 +2,11 @@ function buttonScene1() {
   let title = document.getElementsByClassName("title-container")[0];
 
   let button = document.getElementsByClassName("button-container")[0];
+  let audio = document.getElementById("audio");
+
+  if (audio.muted || audio.error) {
+    audio.play();
+  }
   title.style.visibility = "hidden";
   button.style.visibility = "hidden";
 
